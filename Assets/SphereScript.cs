b81f71bf1445482e3300efbdc6a866233ff0ab09
PingKee <pingkee.ng@gmail.com>
2020-12-22 00:26:57 +0800

    feat: Joystick element added

---
+++ Assets/SphereScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    protected bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal* 10f,
+                                        rigidbody.velocity.y,
+                                        joystick.Vertical * 10f);
+        
+        if (!jump && joybutton.Pressed) {
+            jump = true;
+            rigidbody.velocity += Vector3.up * 10f;
+        }
+        if (jump && !joybutton.Pressed) {
+            jump = false;
+        }
+        // Axis for keyboard
+        // var rigidbody = GetComponent<Rigidbody>();
+        // rigidbody.velocity = new Vector3(joystick.Horizontal* 10f + Input.GetAxis("Horizontal") * 10f,
+        //                                 rigidbody.velocity.y,
+        //                                 joystick.Vertical * 10f + Input.GetAxis("Vertical") * 10f);
+        
+        // if (!jump && (joybutton.Pressed || Input.GetButton("Fire2"))) {
+        //     jump = true;
+        //     rigidbody.velocity += Vector3.up * 10f;
+        // }
+        // if (jump && (!joybutton.Pressed || Input.GetButton("Fire2")) {
+        //     jump = false;
+        // }
+    }
+}
