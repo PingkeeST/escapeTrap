20abbfc8d84f856f7870b85cedcb5b50172563f8
PingKee <pingkee.ng@gmail.com>
2020-12-29 01:05:04 +0800

    update: added menu page and tranversing through scenes

---
+++ Assets/MainManu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManu : MonoBehaviour
+{
+    public void Play () {
+        // two ways to load a scene. with 
+        SceneManager.LoadScene("controllers");
+        // or by build index through build settings
+        // SceneManager.LoadScene(GetActiveScene.buildIndex + 1);
+    }
+    public void QuitGame () {
+        Application.Quit();
+    }
+
+}
