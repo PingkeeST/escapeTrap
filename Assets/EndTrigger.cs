651bb73a6f89e287c30c1f0b67367e947279fa7f
PingKee <pingkee.ng@gmail.com>
2021-01-09 17:30:09 +0800

    feat: end game scene

---
+++ Assets/EndTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager GameManager;
+    
+    // OnTriggerEnter can only be use when item have a collider
+    // and isTrigger must be true
+    void OnTriggerEnter ()
+    {
+        // you get to access the GameManager script and call its funtion
+        // do remember to insert the script to the collision
+        GameManager.ComplateLevel();
+    }
+
+
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject LevelCompletedUI;
+
+    public void ComplateLevel() 
+    {
+        LevelCompletedUI.SetActive(true);
+    }
+}
